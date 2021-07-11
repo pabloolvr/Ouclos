4aa9285594c7af74b5ee56e90dc46e7fe0f5b052
pabloolvr <48020553+pabloolvr@users.noreply.github.com>
2021-07-11 16:02:47 -0300

    loading data from backend

--- frontend/src/screens/HomePage.js
+++ frontend/src/screens/HomePage.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Product from '../components/Product';
-import data from '../data';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+//import data from '../data';
 
 export default function HomePage() {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+                const { data } = await axios.get('/api/products'); // fetch data from backend
+                setLoading(false);
+                setProducts(data);
+            } catch(e) {
+                setError(e.message);
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, [])
     return (
         <div>
-            <div className="row center">
-                {data.products.map((product) => (
-                    <Product key={product._id} product={product}></Product>
-                ))}
-            </div>
+            {loading ? (
+                <LoadingBox></LoadingBox> // show loading box when loading data from backend
+            ) : error ? (
+                <MessageBox variant="danger">{error}</MessageBox> // show message box with error when somethings goes wrong when fetching data from backend
+            ) : (
+                <div className="row center">
+                    {products.map((product) => (
+                        <Product key={product._id} product={product}></Product>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
