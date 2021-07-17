843a79fa434670cba7590084d0aea7685b71ef94
pabloolvr <48020553+pabloolvr@users.noreply.github.com>
2021-07-17 13:13:47 -0300

    created place order api

--- backend/utils.js
+++ backend/utils.js
@@ -14,3 +14,24 @@ export const generateToken = (user) => {
         }
     );
 };
+
+export const isAuth = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (authorization) {
+        // get token from request
+        const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
+        // verify token
+        jwt.verify(token, process.env.JWT_SECRET || 'somethingsecret',
+            (err, decode) => {
+                if (err) {
+                    res.status(401).send({ message: 'Invalid Token' });
+                } else {
+                    req.user = decode; // store user information in req
+                    next();
+                }
+            }
+        );
+    } else {
+        res.status(401).send({ message: 'No Token' });
+    }
+};
